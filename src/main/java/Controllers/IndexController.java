8456cbe73b18ce077cecfb4b64693f82e5cb2465
Emily Bowersox <emily.bowersox100@gmail.com>
2020-07-04 16:34:56 -0500

    'cannot resolve MVC view'

--- src/main/java/Controllers/IndexController.java
+++ src/main/java/Controllers/IndexController.java
@@ -2,12 +2,13 @@ package Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String viewIndex(){
+    public String viewIndex() {
         return "index";
     }
 
