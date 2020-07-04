d7448cae3f26b44004162631bb5b782910d33653
Emily Bowersox <emily.bowersox100@gmail.com>
2020-07-04 16:18:16 -0500

    cleaning up file structure

---
+++ src/main/java/Controllers/IndexController.java
@@ -0,0 +1,16 @@
+package Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String viewIndex(){
+        return "index";
+    }
+
+
+
+}
